f231fbee63287ed313df1e0b5240b38fb5b14df4
Thauan-th <thauanandre09@gmail.com>
2022-05-07 18:27:28 -0300

    trabalhando com tipagem simples de dados

--- praticas/primeiro.js
+++ praticas/primeiro.js
@@ -1,2 +1,22 @@
-var numero = 10;
-console.log(numero);
+var numero = 0;
+var nome = 'Thauan';
+var ligado = false;
+function logParams(params) {
+    console.log(params);
+    return;
+}
+function upperName(name) {
+    logParams(name);
+    return name.toUpperCase();
+}
+function multPer2(num) {
+    logParams(num);
+    return num * 2;
+}
+function toggle(toggle) {
+    logParams(toggle);
+    return !toggle;
+}
+upperName(nome);
+multPer2(numero);
+toggle(ligado);
--- praticas/primeiro.ts
+++ praticas/primeiro.ts
@@ -1,2 +1,26 @@
-let numero: number  =10 
-console.log(numero)
+const numero:number =0
+
+const nome:string = 'Thauan'
+
+const ligado:boolean = false
+
+function logParams(params:any){
+    console.log(params)
+    return
+}
+
+function upperName(name:string){
+    logParams(name)
+    return name.toUpperCase()
+}
+function multPer2(num:number){
+    logParams(num)
+    return num * 2
+}
+function toggle(toggle:boolean){
+    logParams(toggle)
+    return !toggle
+}
+upperName(nome)
+multPer2(numero)
+toggle(ligado)
