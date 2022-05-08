a9d4cb64d0a48c1c0f40ab0809cda478005c7b5a
Thauan-th <thauanandre09@gmail.com>
2022-05-07 21:38:24 -0300

    tipagem em retorno de funcoes

--- praticas/tipos.js
+++ praticas/tipos.js
@@ -4,3 +4,9 @@ var array_string = ['a', 'e', 'i', 'o', 'u'];
 var inference_string = array_string.map(function (item) { return item.toUpperCase(); });
 var array_number_another = [100, 200];
 var array_string_another = ['b', 'a'];
+var num = 10;
+var num2 = 20;
+function sum(num, num2) {
+    return num + num2;
+}
+console.log(sum(num, num2));
--- praticas/tipos.ts
+++ praticas/tipos.ts
@@ -1,6 +1,16 @@
 let array_number:number[] = [10,20,30,40,50]
 let inference_number = array_number.map(item=>item*2)
+
 let array_string:string[] =['a','e','i','o','u']
 let inference_string = array_string.map(item=>item.toUpperCase())
+
 const array_number_another: Array<number> = [100,200]
 const array_string_another: Array<string> = ['b','a']
+
+let num:number = 10
+let num2:number =20
+
+function sum(num:number,num2:number):number{
+    return num + num2
+}
+console.log(sum(num,num2))
